d920955e90e59f0949ac5bb3296681ddf21b456f
qpy <2476083643@qq.com>
2025-03-23 11:56:40 +0800

    第10次提交，lab3完成

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -15,18 +15,18 @@ public class TestBuggyAList {
   // YOUR TESTS HERE
     @Test
     public void testThreeAddThreeRemove() {
-        AListNoResizing<Integer> ANobug = new AListNoResizing<>();
-        BuggyAList<Integer> Bhasbug = new BuggyAList<>();
-        ANobug.addLast(4);
-        Bhasbug.addLast(4);
-        ANobug.addLast(5);
-        Bhasbug.addLast(5);
-        ANobug.addLast(6);
-        Bhasbug.addLast(6);
-
-        Assert.assertEquals(ANobug.size(),Bhasbug.size());
-        Assert.assertEquals(ANobug.removeLast(),Bhasbug.removeLast());
-        Assert.assertEquals(ANobug.removeLast(),Bhasbug.removeLast());
+//        AListNoResizing<Integer> ANobug = new AListNoResizing<>();
+//        BuggyAList<Integer> Bhasbug = new BuggyAList<>();
+//        ANobug.addLast(4);
+//        Bhasbug.addLast(4);
+//        ANobug.addLast(5);
+//        Bhasbug.addLast(5);
+//        ANobug.addLast(6);
+//        Bhasbug.addLast(6);
+//
+//        Assert.assertEquals(ANobug.size(),Bhasbug.size());
+//        Assert.assertEquals(ANobug.removeLast(),Bhasbug.removeLast());
+//        Assert.assertEquals(ANobug.removeLast(),Bhasbug.removeLast());
     }
 
 //    @Test

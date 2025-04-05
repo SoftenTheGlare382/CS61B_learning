dd185ed799c9ad5a521725c367ade416251c798d
qpy <2476083643@qq.com>
2025-04-05 22:25:22 +0800

    prj1完成

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -12,32 +12,33 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
 
     public ArrayDeque() {
         items=(T[])new Object[8];
-        this.capacity=items.length;
-        nextfirst=capacity-1;
-        nextlast=0;
-        size=0;
+        this.capacity = items.length;
+        nextfirst = capacity - 1;
+        nextlast = 0;
+        size = 0;
 
     }
 
     @Override
     public void addFirst(T item) {
         //直接当size等于capacity时调整大小，而不是看两个指针的相对位置
-        if (size==capacity)
-            resize(capacity*2);
-        items[nextfirst]=item;
+        if (size == capacity)
+            resize(capacity * 2);
+        items[nextfirst] = item;
         size++;
         //nextFirst有可能越界
-        nextfirst=nextfirst==0?capacity-1:nextfirst-1;
+        nextfirst = nextfirst == 0?capacity - 1 : nextfirst - 1;
     }
 
     @Override
     public void addLast(T item) {
-        if (size==capacity)
-            resize(capacity*2);
+        if (size == capacity) {
+            resize(capacity * 2);
+        }
         items[nextlast]=item;
         size++;
         //nextLast有可能越界
-        nextlast=(nextlast+1)%capacity;
+        nextlast = (nextlast+1)%capacity;
 
     }
 
@@ -49,20 +50,20 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
     @Override
     public void printDeque() {
         //nextFirst有可能指向最后一个位置
-        for (int i=(nextfirst+1)%capacity;i!=nextlast-1;i=(i+1)%capacity)
+        for (int i=(nextfirst+1)%capacity;i != nextlast-1;i = (i + 1) % capacity)
             System.out.print(items[i]+" ");
-        System.out.print(items[nextlast-1]);
+        System.out.print(items[nextlast - 1]);
     }
 
     @Override
     public T removeFirst() {
         //当数组的内容为空的时候，才无法进行remove操作，而不是取决于nextFirst的位置。
-        if (size==0)return null;
-        nextfirst=(nextfirst+1)%capacity;
+        if (size == 0){return null;}
+        nextfirst=(nextfirst + 1)%capacity;
         T temp=items[nextfirst];
-        items[nextfirst]=null;
+        items[nextfirst] = null;
         size--;
-        if (capacity>=16&&size<capacity/4)
+        if (capacity >= 16&&size < capacity / 4)
             resize(capacity/2);
         return temp;
 
@@ -70,12 +71,12 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
 
     @Override
     public T removeLast() {
-        if (size==0)return null;
+        if (size == 0)return null;
         nextlast = (nextlast - 1 + capacity) % capacity; // ✅ 处理环形索引
-        T temp=items[nextlast];
-        items[nextlast]=null;
+        T temp = items[nextlast];
+        items[nextlast] = null;
         size--;
-        if (items.length>=16&&size<items.length/4)
+        if (items.length >= 16 && size < items.length/4)
             resize(items.length/2);
         return temp;
 
@@ -117,13 +118,13 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
         //由于nextFirst和nextLast的位置不确定，只能一个一个地复制到新的数组中
         //从nextFirst右边的第一个点开始复制
         //到nextLast左边的第一个点复制结束
-        for (int i=1;i<=size;i++)
-            a[i]=items[(++nextfirst)%this.capacity];
-        this.capacity=capacity;
+        for (int i = 1;i <= size;i++)
+            a[i] = items[(++nextfirst)%this.capacity];
+        this.capacity = capacity;
         //这两个指针指向什么地方已经不重要了
-        nextfirst=0;
-        nextlast=size+1;
-        items=a;
+        nextfirst = 0;
+        nextlast = size+1;
+        items = a;
     }
 
     public boolean equals(Object o){
